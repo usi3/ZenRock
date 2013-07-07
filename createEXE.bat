6677ac8cfd6661ac6d51663b07e7faae5005d038
Naoki Ishikawa <tetuet@gmail.com>
2013-07-07 19:16:40 +0900

    Refactoring

--- createEXE.bat
+++ createEXE.bat
@@ -1,7 +1,7 @@
 set LANG=ja_JP.UTF-8
-set ocrapath=%ocrapath%
-call %ocrapath% --no-lzma check_setting.rb
-icacls check_setting.exe /grant Everyone:RX
+set ocrapath="C:\Program Files (x86)\Ruby-2.0.0\bin\ocra"
+call %ocrapath% --no-lzma checksetting.rb
+icacls checksetting.exe /grant Everyone:RX
 call %ocrapath% --no-lzma main.rb
 icacls main.exe /grant Everyone:RX
 call %ocrapath% --no-lzma createthumbnail.rb
